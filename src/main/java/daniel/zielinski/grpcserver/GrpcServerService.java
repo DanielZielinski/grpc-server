22b65dff8219d762769bdce5868e0b2662be4edd
DanielZielinski <dzielinski@9bits.pl>
2023-03-15 22:38:44 +0100

    init commit

---
+++ src/main/java/daniel/zielinski/grpcserver/GrpcServerService.java
@@ -0,0 +1,21 @@
+package daniel.zielinski.grpcserver;
+
+import daniel.zielinski.grpc.HelloGrpc;
+import daniel.zielinski.grpc.HelloReply;
+import daniel.zielinski.grpc.HelloRequest;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+@Slf4j
+public class GrpcServerService extends HelloGrpc.HelloImplBase {
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        log.info("received request");
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
---
+++ src/test/java/daniel/zielinski/grpcserver/GrpcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.zielinski.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
